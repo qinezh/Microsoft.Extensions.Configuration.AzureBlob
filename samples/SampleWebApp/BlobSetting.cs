df97d86391b91d23778f08f51f281dc33e69aeaf
Qinen Zhu <qinzhu@microsoft.com>
2020-03-26 09:48:34 +0800

    Add sample project

---
+++ samples/SampleWebApp/BlobSetting.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SampleWebApp
+{
+    public class BlobSetting
+    {
+        public string BlobUrl { get; set; }
+
+        public Dictionary<string, string> Metadata { get; set; }
+    }
+}
---
+++ samples/SampleWebApp/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace SampleWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly BlobSetting _options;
+
+        public HomeController(IOptionsSnapshot<BlobSetting> options)
+        {
+            _options = options.Value;
+        }
+
+        public IActionResult Index()
+        {
+            var config = new Dictionary<string, string>
+            {
+                {"BlobUrl",  _options.BlobUrl ?? "undefined" }
+            };
+
+            if (_options.Metadata != null)
+            {
+                foreach (var (key, value) in _options.Metadata)
+                {
+                    config.Add($"metadata:{key}", value);
+                }
+            }
+
+            ViewData["config"] = config;
+
+            return View();
+        }
+    }
+}
